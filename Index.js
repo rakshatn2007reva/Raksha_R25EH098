2b0b0d7c8cbdd43588a90f6f68ce0fbdae166654
rakshatn2007reva <rakshatn2007reva@gmail.com>
2026-09-24 12:25:52 +0530

    Fetch GitHub user data using axiosAdd axios request

--- Index.js
+++ Index.js
@@ -1,1 +1,15 @@
+const axios = require("axios");
 
+async function getGitHubUser() {
+  try {
+    const response = await axios.get(
+      "https://api.github.com/users/octocat"
+    );
+
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching GitHub user:", error.message);
+  }
+}
+
+getGitHubUser();
